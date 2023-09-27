0b78365d8886911fbb3cd980b6b8eb2610945fd1
DESKTOP-PR2BK7P\csi23-mserina <csi23-mserina@colegioaltair.net>
2023-09-27 12:54:42 +0200

    Agregar archivos de proyecto.

---
+++ Servicios/MenuImplementacion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuPersonal.Servicios
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+        public void mensajeBienvenida()
+        {
+            string saludo = "Bienvenido Warrior Game";
+            Console.WriteLine(saludo);
+        }
+
+        public int mostrarmostrarMenuYOpciones()
+        {
+            int opcionSeleccionada;
+            Console.WriteLine("###############################");
+            Console.WriteLine("0. JUGAR");
+            Console.WriteLine("1. AJUSTES");
+            Console.WriteLine("2. SALIR");
+            Console.WriteLine("###############################");
+
+
+        }
+    }
+}
---
+++ Servicios/MenuInterfaz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuPersonal.Servicios
+{
+    /// <summary>
+    /// La interfaz donde se hara referencia a toda las acciones que se pueden hacer en el menu
+    /// 27/09/23 - msm
+    /// </summary>
+    internal interface MenuInterfaz
+    {
+        /// <summary>
+        /// Llama al metodo mensajeBienvenida desde menuImplementacion ( cual se espicfica su funcion)
+        /// 27/09/23 - msm
+        /// </summary>
+        public void mensajeBienvenida();
+        
+        /// <summary>
+        /// Este metodo llama al menu y recoge la respuesta del usuario
+        /// 27/09/23 - msm
+        /// </summary>
+        
+        public int mostrarmostrarMenuYOpciones();
+        
+    }
+}
